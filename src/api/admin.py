6197c91e6fb1070825d5604549e10d7a1bf8b735
truffel <tomsonrz@protonmail.ch>
2023-12-20 00:53:29 +0100

    Admin sort

--- src/api/admin.py
+++ src/api/admin.py
@@ -4,4 +4,12 @@ from .models import *
 # Register your models here.
 
 
-admin.site.register(NewsObject)
+#news admin
+class NewsObjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'guid', 'type', 'title_pl', 'link', 'description_pl', 'pub_date', 'image', 'created_at', 'updated_at', 'title_eng', 'description_eng', 'translated')
+    list_display_links = ('id', 'guid', 'type', 'title_pl', 'link', 'description_pl', 'pub_date', 'image', 'created_at', 'updated_at', 'title_eng', 'description_eng', 'translated')
+    list_filter = ('type', 'translated')
+    search_fields = ('type', 'title_pl', 'link', 'description_pl', 'pub_date', 'image', 'title_eng', 'description_eng')
+    list_per_page = 25
+
+admin.site.register(NewsObject, NewsObjectAdmin)
